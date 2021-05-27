ae64999303c70de963d71e6362763b422b9264b9
Alina <a.gerash@bk.ru>
2021-05-27 13:40:01 +0400

    Fix images on project page

--- src/main/js/pages/ProjectPage.js
+++ src/main/js/pages/ProjectPage.js
@@ -3,40 +3,47 @@ import React from 'react';
 import DefaultHeader from "../components/DefaultHeader";
 import Footer from "../components/Footer";
 
+import image1 from '../../resources/static/img/1/1.jpg';
+import image2 from '../../resources/static/img/1/2.jpg';
+import image3 from '../../resources/static/img/1/10.jpg';
+import image4 from '../../resources/static/img/2/3.jpg';
+import image5 from '../../resources/static/img/2/4.jpg';
+import image6 from '../../resources/static/img/2/6.jpg';
+
 import '../../resources/static/style.css';
 
 
 export const ProjectPage = () => {
     return (
         <>
-            <DefaultHeader text="Наши проекты" />
+            <DefaultHeader text="Наши проекты"/>
             <div className="container1">
                 <div className="box1">
-                        <img alt src="img/1/1.jpg"/>
+                    <img alt src={image1}/>
                 </div>
                 <div className="box1">
-                    <img alt src="img/1/4.jpg"/>
+                    <img alt src={image2}/>
 
                 </div>
                 <div className="box1">
-                    <img alt src="img/1/8.jpg"/>
+                    <img alt src={image3}/>
 
                 </div>
                 <div className="box1">
-                    <img alt src="img/1/6.jpg"/>
+                    <img alt src={image4}/>
 
                 </div>
                 <div className="box1">
-                    <img alt src="img/2.jpg"/>
+                    <img alt src={image5}/>
 
                 </div>
                 <div className="box1">
-                    <img alt src="img/4.jpg"/>
+                    <img alt src={image6}/>
 
                 </div>
             </div>
 
-            <Footer />
+            <Footer/>
         </>
 
     )
